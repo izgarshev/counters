47bed43ada6548e9bb88aa770476b1abb93323d0
izgarshev <izgarshev_p@mail.ru>
2021-05-18 19:24:52 +0300

    start

---
+++ src/main/java/org/pablo/model/Counter.java
@@ -0,0 +1,4 @@
+package org.pablo.model;
+
+public class Counter {
+}
---
+++ src/main/java/org/pablo/repos/CounterRepo.java
@@ -0,0 +1,8 @@
+package org.pablo.repos;
+
+import org.pablo.model.Counter;
+
+public interface CounterRepo {
+    void createCounter(Counter counter);
+
+}
